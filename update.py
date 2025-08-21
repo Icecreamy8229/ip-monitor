154a036b1ce5baf1337db9308a0d10bbb7590ada
KeroKeroKaeru <107955291+KeroKeroKaeru@users.noreply.github.com>
2025-08-21 00:16:26 -0400

    Refactor update.py and version.json fix
    pointed to the public repo
    function created to get the latest release information as a tuple
    added checks to run test updates in dev environment.
    wrapped a few file writes in try except blocks
    version was missing a "v" in front of it.

--- update.py
+++ update.py
@@ -12,18 +12,33 @@ from dotenv import load_dotenv
 
 
 init(autoreset=True)
-OWNER = "YourUsername"
-REPO = "IpMonitor"
-DOWNLOAD_URL = f"https://github.com/{OWNER}/{REPO}/releases/latest/download/ipmonitor.zip"
-VERSION_URL = f"https://raw.githubusercontent.com/{OWNER}/{REPO}/main/version.json"
-VERSION = "0.0.0"
-BRANCH = "main"
+OWNER = "Icecreamy8229"
+REPO = "ip-monitor"
+working_dir = os.getcwd()
+TEST_DIR = "testing"
+
+def get_latest_release_info():
+    """Uses GitHub API to get latest release tag and zip URL"""
+    url = f"https://api.github.com/repos/{OWNER}/{REPO}/releases/latest"
+    r = requests.get(url)
+    r.raise_for_status()
+    data = r.json()
+    tag = data["tag_name"] #this is the version
+    zip_name = f"{REPO}-{tag}.zip"
+    zip_url = f"https://github.com/{OWNER}/{REPO}/releases/latest/download/{zip_name}"
+    return tag, zip_url
+
+
+VERSION, DOWNLOAD_URL = get_latest_release_info()
 
 
 def get_local_version():
-    with open("version.json", "r") as version_file:
-        contents = json.load(version_file)
-        return contents["version"]
+    try:
+        with open("version.json", "r") as version_file:
+            contents = json.load(version_file)
+            return contents["version"]
+    except FileNotFoundError:
+        return "0.0.0"
 
 def download_files():
     print(Fore.YELLOW + "Downloading latest version.")
@@ -32,6 +47,9 @@ def download_files():
     content_disposition = r.headers.get('Content-Disposition',"")
     filename_match = re.findall('filename="?([^"]+)"?', content_disposition)
     filename = filename_match[0] if filename_match else None
+    if is_test_update():
+        with open(filename_match[0], "wb") as local_file:
+            local_file.write(r.content)
     with open(filename_match[0], "wb") as local_file:
         local_file.write(r.content)
     return filename
@@ -53,12 +71,14 @@ def save_previous_config():
     else:
         backup_files = os.listdir("./backup")
 
+    try:
+        with open("config.yaml", "r") as config_file:
+            contents = config_file.read()
 
-    with open("config.yaml", "r") as config_file:
-        contents = config_file.read()
-
-    with open(f"./backup/{len(backup_files)}-config.yaml", "a+") as backup_config:
-        backup_config.write(contents)
+        with open(f"./backup/{len(backup_files)}-config.yaml", "a+") as backup_config:
+            backup_config.write(contents)
+    except FileNotFoundError:
+        log_output("config.yaml not found.  Skipping backup.")
 
 def restore_previous_config():
     log_output("restoring previous config file")
@@ -74,24 +94,23 @@ def restore_previous_config():
 
 
 
+
+
 def compare_versions():
     print("Checking for Updates...")
-    r = requests.get(VERSION_URL)
-    r = r.json()
-    server_version = r['version']
+    server_version = VERSION
     local_version = get_local_version()
     if server_version != local_version:
         print(Fore.YELLOW + "Update Found!")
         log_output("Server version is different from local version")
-        if not is_test_update():
-            save_previous_config()
+        save_previous_config()
         filename = download_files()
-        if not is_test_update():
-            unzip_file(filename)
-            restore_previous_config()
-            log_output("removing downloaded zip file.")
-            os.remove(filename)
-            print(Fore.GREEN + "Successfully Updated!")
+        unzip_file(filename)
+        restore_previous_config()
+        log_output("removing downloaded zip file.")
+        os.remove(filename)
+        print(Fore.GREEN + "Successfully Updated!")
+
 
     else:
         print(Fore.GREEN + "No update found!")
@@ -107,13 +126,20 @@ def is_test_update():
 
 
 def main():
+    global working_dir
     if is_test_update():
-        print("Test Update Enabled.")
+        print(Fore.RED + "Test Update Enabled.")
+        create_testing_dir()
+        working_dir = os.path.join(working_dir, TEST_DIR)
+        os.chdir(working_dir)
 
     compare_versions()
 
+def create_testing_dir():
+    os.makedirs(TEST_DIR, exist_ok=True)
 
 if __name__ == "__main__":
+
     if load_dotenv(dotenv_path='.env') and not is_test_update():
         print("Development ENV loaded, skipping version check.")
         time.sleep(2)
